f4b5037c6712ddee80c2896d35c1af126392be41
vagrant <vagrant@homestead>
2018-04-02 17:18:34 +0000

    sth useful

---
+++ src/Sunil.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sunil
+
+class Sunil
+{
+    /**
+        Randome seed
+    */
+    protected $a = 0;
+
+    public function run()
+    {
+        echo "Okay got it";
+    }
+
+
+}
